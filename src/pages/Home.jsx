80ff3461be8545d3865bf986d41094b9e246a36b
User 1 <user1@Users-MacBook-Pro.local>
2025-07-06 16:29:54 -0700

    First Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export const Home = () => {
+    return <div className="min-h-screen">
+        {/* Theme Toggle */}
+
+        {/* Background Effects */}
+
+        {/* NavBar */}
+
+        {/* Main Content */}
+        
+           </div>;
+}
